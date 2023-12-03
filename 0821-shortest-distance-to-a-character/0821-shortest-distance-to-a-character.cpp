77f61b59edfff97a92d4a251021995b346107f70
sahana <64575684+Espinosa-12@users.noreply.github.com>
2023-12-03 23:41:26 +0530

    Time: 4 ms (45.90%), Space: 7.3 MB (10.96%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
@@ -20,5 +20,8 @@ public:
             if(x<v.size()-1 && v[x]<=i) x++;
         }
         return ans;
+        
+        /*there's another approach to this question .like you don't need to use extra space 
+        to store all the position of the char in a vector. instead you can subtract and find the position then and there itself.try doing it*/
     }
 };
